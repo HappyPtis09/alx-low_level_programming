39eb21a12fe86d4836a90dd6cd5a13c7ada1d8b0
HappyPtis09 <bokunozaq@gmail.com>
2023-07-13 09:32:03 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
