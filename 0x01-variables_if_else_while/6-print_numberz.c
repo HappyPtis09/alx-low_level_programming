393982b7dd645e8c4809a4b86a5661d7ab90155d
HappyPtis09 <bokunozaq@gmail.com>
2023-07-14 13:00:42 -0700

    lololol

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-11;rgb:0000/0000/0000/**
-11;rgb:0000/0000/0000* main - Entry point
+/**
+ * main - Entry point
  *
  * Description: Prints all the single digit numbers of base 10 starting from 0,
  *              followed by a new line.
