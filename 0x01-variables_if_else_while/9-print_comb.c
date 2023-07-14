72502d4cebe2b474b6271e9c3f1e715f206e53da
HappyPtis09 <bokunozaq@gmail.com>
2023-07-14 15:54:12 -0700

    finally

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i = 0;
 
 	while (i < 10)
 	{
 		putchar(i + '0');
-		i++;
-	}
-	if (i != 9)
+		if (i != 9)
 	{
 		putchar(',');
 		putchar(' ');
+	}
+		i++;
 	}
 	return (0);
 }
