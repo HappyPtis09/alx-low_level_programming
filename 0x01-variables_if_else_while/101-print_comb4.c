ed33702b10eaba24660dbeb5df41b706d51ad2f1
HappyPtis09 <135461931+HappyPtis09@users.noreply.github.com>
2023-07-15 17:28:28 +0100

    testing

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	int n, m, l;
+	int i, j, k;
 
-	for (n = 48; n < 58; n++)
+	for (i = 48; i < 58; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for (j = 49; j < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (k = 50; k < 58; l++)
 			{
-				if (l > m && m > n)
+				if (k > j && j > i)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
 					{
 						putchar(',');
 						putchar(' ');
