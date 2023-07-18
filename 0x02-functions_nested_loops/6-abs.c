fb8af12045ed0f1218d75ede9294af8a0dbadd4a
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 10:02:40 -0700

    h

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * _abt - absolute value for integer
- * @n: the int to check
- * Return: the absolute value
- */
-int _abs(int)
+* _abt - absolute value for integer
+* @n: the int to check
+* Return: the absolute value
+*/
+int _abs(int n)
 {
-	int n;
-
 	if (n < 0)
 	{
 		_putchar(-n);
