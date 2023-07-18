4bf9a3bd73d324fd1c3f57700260e702fef1d1fa
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 13:32:59 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,26 @@
 */
 void print_to_98(int n)
 {
-	n = 0;
+	int i, j;
 
-	while (n < 98)
+	if (n <= 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
 	}
-	n++;
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
