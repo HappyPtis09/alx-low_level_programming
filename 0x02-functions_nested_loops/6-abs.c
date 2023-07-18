fa537ff7283f8894c15b7781f94a6df0f0fc2af2
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 09:57:04 -0700

    h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abt - absolute value for integer
+ * @n: the int to check
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		_putchar(-n);
+	}
+	else
+	{
+		_puthar(n);
+	}
+}
