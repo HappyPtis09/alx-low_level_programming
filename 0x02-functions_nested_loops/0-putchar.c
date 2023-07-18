eeee61d3a0915af52ff9080266329c910b9a6777
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 03:46:32 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Enty point
+ *
+ * program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
