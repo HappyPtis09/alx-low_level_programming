bc5aaf5634298a5cf45014f02b5463db7239e968
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 11:02:08 -0700

    h

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-	{
 		n = -n;
-		a = n % 10;
-		_putchar(a + '0');
-	}
+	a = n % 10;
+		if (a < 0)
+			a = -a;
+		_puthar(a + '0');
 	return (a);
 }
