602afde9cc5c6b55ed9b699066685c8f4a4fe5a1
HappyPtis09 <bokunozaq@gmail.com>
2023-07-18 11:51:47 -0700

    task1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,21 +8,24 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	while (i < 24)
-	{
+
 		i = 0;
 
-		while (j < 60);
+
+		while (i < 24)
 		{
 			j = 0;
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			++j;
+			while (j < 60)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+				j++;
+			}
+			i++;
 		}
-		i++;
-	}
+
 }
