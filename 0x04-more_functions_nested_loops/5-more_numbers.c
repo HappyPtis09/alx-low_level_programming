4a2b2de3cece05011502101162dcf8752f66cc2c
HappyPtis09 <bokunozaq@gmail.com>
2023-07-20 13:46:49 -0700

    tas5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,16 @@
 
 void more_numbers(void)
 {
-  int i = 0, j;
+	int i, j;
 
-  while (i <= 10)
-  {
-    j = 0;
-    while (j <= 14)
-    {
-      if (j >= 10)
-        _putchar(j / 10 + '0');
-      _putchar(j % 10 + '0');
-      j++;
-    }
-    _putchar('\n');
-    i++;
-  }
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
