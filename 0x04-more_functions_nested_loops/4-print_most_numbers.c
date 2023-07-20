83b3accfc90d023a5591419871af0c06ea9cf2e9
HappyPtis09 <bokunozaq@gmail.com>
2023-07-20 06:19:09 -0700

    waaaaa

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -15,9 +15,7 @@ void print_most_numbers(void)
 	while (i >= '0' && i <= '9')
 	{
 		if (!(i == '2' || i == '4'))
-		{
-			_putchar(i + '0');
-		}
+			_putchar(i + '0')
 		i++;
 	}
 	_putchar('\n');
