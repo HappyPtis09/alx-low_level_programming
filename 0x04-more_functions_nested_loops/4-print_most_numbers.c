040bf17df50a46d8ef49a4ea13e436422c2b51b4
HappyPtis09 <bokunozaq@gmail.com>
2023-07-20 06:14:31 -0700

    damn

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
+#include "main.h"
 
-0;276;0c#include "main.h"
 /**
  * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
  *
@@ -8,11 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (i >= '0' && i <= '9')
 	{
-		if (!(i == '2' && i =='4'))
+		if (!(i == '2' || i == '4'))
 		{
 			_putchar(i + '0');
 		}
