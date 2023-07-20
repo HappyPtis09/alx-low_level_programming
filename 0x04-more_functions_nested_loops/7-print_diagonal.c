a469f558abe60e2959b7f48680ad6ea4fba7d1b2
HappyPtis09 <bokunozaq@gmail.com>
2023-07-20 08:10:40 -0700

    tas7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,10 +13,17 @@ void print_diagonal(int n)
 
 		while (i <= n)
 		{
+			int j = 1;
+
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
 			_putchar('\\');
+			_putchar('\n');
 			i++;
 		}
-		_putchar('\n');
 	}
 	else
 	{
