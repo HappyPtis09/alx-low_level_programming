aa6d0b21450ef8b53890672e22cc326ef3affd6a
HappyPtis09 <bokunozaq@gmail.com>
2023-07-20 15:45:45 -0700

    unbelievable

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,21 +18,20 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
-		printf(" ");
 		i++;
 	}
 	printf("\n");
