231b670427917b7ea3a56a0d758b032d40cd2eb0
HappyPtis09 <bokunozaq@gmail.com>
2023-07-24 12:43:33 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,22 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-	
+
 	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
-	
+
 	while (s[len] != '\0')
 		len++;
-	
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -38,9 +38,9 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	
+
 	if (f == 0)
 		return (0);
-	
+
 	return (n);
 }
