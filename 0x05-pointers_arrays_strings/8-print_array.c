38f81e6834af02275acb8853d3370ba4ec5353f4
HappyPtis09 <bokunozaq@gmail.com>
2023-07-24 10:49:12 -0700

    yay

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,11 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		printf("%d", a[i])
-			if (i != n - 1)
-			{
-				printf(", ");
-			}
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
 		i++;
 	}
 	printf("\n");
