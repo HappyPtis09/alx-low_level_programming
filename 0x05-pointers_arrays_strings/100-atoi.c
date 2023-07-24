fc39182c950020ee242605d3cebf0159c0280d9c
HappyPtis09 <bokunozaq@gmail.com>
2023-07-24 12:39:20 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int n = 0, sign = 1;
+	int i, d, n, len, f, digit;
 	
-	while (*s != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	
+	while (s[len] != '\0')
+		len++;
+	
+	while (i < len && f == 0)
 	{
-		if (*s == '-')
-		{
-			sign *= -1;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			n = n * 10 + (*s - '0');
-		}
-		else if (n > 0)
-		{
-			break;
-		}
+		if (s[i] == '-')
+			++d;
 		
-		s++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
 	
-	return n * sign;
+	if (f == 0)
+		return (0);
+	
+	return (n);
 }
