59792067f68aa67bfbdce15b3c27f75e2298aa25
HappyPtis09 <bokunozaq@gmail.com>
2023-07-27 07:23:00 -0700

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @i: pointer
+ *
+ * Return: i
+ */
+
+char *string_toupper(char *i)
+{
+	char *j = i;
+
+	while (*j != '\0')
+	{
+		if (*j >= 'a' && j <= 'z')
+		{
+			*j = *j - 'a' + 'A';
+		}
+		j++;
+	}
+	return (i);
+}
