8d877a46929e6eabcd8d2c62084bfb6c6b866ea7
HappyPtis09 <bokunozaq@gmail.com>
2023-07-27 15:09:58 -0700

    yolooooo

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (str[i] == in[j])
+			{
+				str[i] = out[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
