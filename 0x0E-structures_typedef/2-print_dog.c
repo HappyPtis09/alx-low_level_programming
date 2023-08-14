3667954806bc6eb1123d41c41178f29f3fea342f
HappyPtis09 <bokunozaq@gmail.com>
2023-08-14 10:22:09 -0700

    what6

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,24 +11,12 @@
 
 void print_dog(struct dog *d)
 {
-	d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
 	{
 		return;
 	}
-	if (name == NULL)
-		return;
-	if (age == NULL)
-		return;
-	if (owner == NULL)
-		return;
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	printf("Name: %s\n", name);
-	printf("Age: %lf\n", age);
-	printf("Owner: %s\n", name);
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
