5945397c8f19b7865f64d74f02015da2ad328138
HappyPtis09 <bokunozaq@gmail.com>
2023-08-14 09:34:20 -0700

    what2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 };
 
 /**
- * dog_alis - typedef for struct dog
+ * dog_alis - typedef
  */
 typedef struct dog dog_alis;
 
@@ -27,6 +27,5 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
