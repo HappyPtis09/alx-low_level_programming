630091123778ca4de3547952caba54483e0d0330
HappyPtis09 <bokunozaq@gmail.com>
2023-08-16 15:10:14 -0700

    tasl

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include "3-calc.h"
-#include "function_pointers.h"
+
 /**
- * main - performs simple operations
- * @argc: number of command line arguments
- * @argv: array of command line arguments
+ * main - simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
+	int num1, num2, result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (90);
+		return (98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
 
 	if (op_func == NULL)
@@ -29,13 +29,13 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 
-	if (*argv[2] == '/' || *argv[2] == '%')
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
 
-	result = op_func(n1, n2);
+	result = op_func(num1, num2);
 	printf("%d\n", result);
 
 	return (0);
