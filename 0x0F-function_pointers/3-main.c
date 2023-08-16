1c42cd4911e703137c9af9616ee96f27e1861312
HappyPtis09 <bokunozaq@gmail.com>
2023-08-16 11:45:45 -0700

    3-op_functions update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
+#include "function_pointers.h"
 /**
  * main - performs simple operations
  * @argc: number of command line arguments
  * @argv: array of command line arguments
  *
- * Return: 0
+ * Return: 0 on success, 98 on wrong number of arguments,
+ *         99 on unrecognized operator, 100 on division/modulo by 0
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
-	int (*op_func)(int, int);
+    int num1, num2, result;
+    int (*op_func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (90);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (90);
+    }
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		return (99);
-	}
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		return (100);
-	}
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
 
-	result = op_func(n1, n2);
-	printf("%d\n", result);
+    result = op_func(num1, num2);
+    printf("%d\n", result);
 
-	return (0);
+    return (0);
 }
