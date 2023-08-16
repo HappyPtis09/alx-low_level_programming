15a0846b41f537ed8c87f78757446ed52d60d6e1
HappyPtis09 <bokunozaq@gmail.com>
2023-08-16 15:00:27 -0700

    tasl

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,13 +23,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (*(ops[i].op) == *s && *(s + 1) == '\0')
-		{
-			return (ops[i].f);
-		}
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
