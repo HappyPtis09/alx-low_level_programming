312b91b303f6e2d580bf464271aaaf9d97fbc8fb
HappyPtis09 <bokunozaq@gmail.com>
2023-08-31 21:34:54 +0000

    betty error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
 /**
  * flip_bits -  a function that returns the number of bits you would
  *                    need to flip to get from one number to another
- * 
+ *
  * @n: first int
  * @m: second int
  *
