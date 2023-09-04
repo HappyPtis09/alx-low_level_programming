765f576fce57c8392af41b9205a842643bb56c58
HappyPtis09 <bokunozaq@gmail.com>
2023-09-04 18:21:15 +0000

    TASK3 clean

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,38 +16,52 @@ int main(int argc, char *argv[])
 	ssize_t r, w;
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
+	}
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
 
 	while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		w = write(file_to, buffer, r);
 		if (w == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
+		}
 	}
 
 	if (r == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 
 	if (close(file_from) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
+	}
 
 	if (close(file_to) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
+	}
 
 	return (0);
 }
