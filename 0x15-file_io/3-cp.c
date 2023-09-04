699ea4997381524862d83d8b042fc02daa5ca0a4
HappyPtis09 <bokunozaq@gmail.com>
2023-09-04 17:50:58 +0000

    TASK3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+/**
+ * main - a program that copies the content of a file to another file
+ *
+ * @argc: num of arguments
+ * @argv: array
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	int file_from = open(argv[1], O_RDONLY);
+
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	int file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	char buffer[BUFFER_SIZE];
+
+	ssize_t r;
+	ssize_t w;
+
+
+	while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(file_to, buffer, r);
+
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
